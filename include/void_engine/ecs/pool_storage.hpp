bcb65928e3c9324b3355176acbebf64300b9e2a2
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-05-19 22:17:27 +0300

    refactor: pool storage getting and creating pools into a private method

--- include/void_engine/ecs/pool_storage.hpp
+++ include/void_engine/ecs/pool_storage.hpp
@@ -16,56 +16,66 @@ class PoolStorage {
 	public:
 	template <typename Component, typename... Args>
 	Component* create(Entity entity, Args&&... args) {
-		ComponentID id = get_component_id<Component>();
-		if (_pools.find(id) == _pools.end()) {
-			_pools[id] = new Pool<Component>();
-		}
-		return static_cast<Pool<Component>*>(_pools[id])
-			->create(entity, std::forward<Args>(args)...);
+		Pool<Component>* pool = get_or_create_pool<Component>();
+		return pool->create(entity, std::forward<Args>(args)...);
 	}
 
 	template <typename Component>
 	void destroy(Entity entity) {
-		ComponentID id = get_component_id<Component>();
-		if (_pools.find(id) == _pools.end()) return;
-		static_cast<Pool<Component>*>(_pools[id])->destroy(entity);
+		Pool<Component>* pool = get_pool<Component>();
+		if (pool == nullptr) return;
+		pool->destroy(entity);
 	}
 
 	void destroy(Entity entity);
 
 	template <typename Component>
 	void clear() {
-		ComponentID id = get_component_id<Component>();
-		if (_pools.find(id) == _pools.end()) return;
-		static_cast<Pool<Component>*>(_pools[id])->clear();
+		Pool<Component>* pool = get_pool<Component>();
+		if (pool == nullptr) return;
+		pool->clear();
 	}
 
 	template <typename Component>
 	Component* get_component(Entity entity) {
-		ComponentID id = get_component_id<Component>();
-		if (_pools.find(id) == _pools.end()) return nullptr;
-		return static_cast<Pool<Component>*>(_pools[id])->get(entity);
+		Pool<Component>* pool = get_pool<Component>();
+		if (pool == nullptr) return nullptr;
+		return pool->get(entity);
 	}
 
 	template <typename Component>
 	const std::vector<Entity>& get_entities() const {
 		static const std::vector<Entity>& empty = {};
-		ComponentID id = get_component_id<Component>();
-		if (_pools.find(id) == _pools.end()) return empty;
-		return static_cast<Pool<Component>*>(_pools.at(id))->get_entities();
+		Pool<Component>* pool = get_pool<Component>();
+		if (pool == nullptr) return empty;
+		return pool->get_entities();
 	}
 
 	const std::vector<Entity> get_entities() const;
 
 	template <typename Component>
 	bool contains(Entity entity) const {
-		ComponentID id = get_component_id<Component>();
-		if (_pools.find(id) == _pools.end()) return false;
-		return static_cast<Pool<Component>*>(_pools.at(id))->contains(entity);
+		Pool<Component>* pool = get_pool<Component>();
+		if (pool == nullptr) return false;
+		return pool->contains(entity);
 	}
 
 	private:
 	std::unordered_map<ComponentID, PoolBase*> _pools;
+
+	private:
+	template <typename Component>
+	Pool<Component>* get_or_create_pool() {
+		ComponentID id = get_component_id<Component>();
+		if (_pools.find(id) == _pools.end()) _pools[id] = new Pool<Component>();
+		return static_cast<Pool<Component>*>(_pools[id]);
+	}
+	template <typename Component>
+	Pool<Component>* get_pool() const {
+		ComponentID id = get_component_id<Component>();
+		if (_pools.find(id) == _pools.end()) return nullptr;
+		return static_cast<Pool<Component>*>(_pools.at(id));
+	}
 };
 
 }  // namespace void_engine::ECS
