162224691822e46e57bab8a3e5ffca50c4c8d5fd
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-07-16 14:07:19 +0300

    refactor: texture to use DSA

--- include/void_engine/resources/texture.hpp
+++ include/void_engine/resources/texture.hpp
@@ -1,133 +1,216 @@
 #ifndef VOID_ENGINE_RESOURCES_TEXTURE_HPP
 #define VOID_ENGINE_RESOURCES_TEXTURE_HPP
 
-#include <glad/gl.h>
 #include <glm/ext/vector_float2.hpp>
 #include <glm/ext/vector_float4.hpp>
 
 namespace void_engine::resources {
 
 enum class TextureTarget {
-	texture_1d = GL_TEXTURE_1D,
-	texture_2d = GL_TEXTURE_2D,
-	texture_3d = GL_TEXTURE_3D,
-	texture_1d_array = GL_TEXTURE_1D_ARRAY,
-	texture_2d_array = GL_TEXTURE_2D_ARRAY,
-	texture_rectangle = GL_TEXTURE_RECTANGLE,
-	texture_cube_map = GL_TEXTURE_CUBE_MAP,
-	texture_cube_map_array = GL_TEXTURE_CUBE_MAP_ARRAY,
-	texture_2d_multisample = GL_TEXTURE_2D_MULTISAMPLE,
-	texture_2d_multisample_array = GL_TEXTURE_2D_MULTISAMPLE_ARRAY,
+	texture_1d = 0x0DE0,
+	texture_2d = 0x0DE1,
+	texture_3d = 0x806F,
+	texture_1d_array = 0x8C18,
+	texture_2d_array = 0x8C1A,
+	texture_rectangle = 0x84F5,
+	texture_cube_map = 0x8513,
+	texture_cube_map_array = 0x9009,
+	texture_2d_multisample = 0x9100,
+	texture_2d_multisample_array = 0x9102,
 
-	texture_cube_map_positive_x = GL_TEXTURE_CUBE_MAP_POSITIVE_X,
-	texture_cube_map_positive_y = GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
-	texture_cube_map_positive_z = GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
-	texture_cube_map_negative_x = GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
-	texture_cube_map_negative_y = GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
-	texture_cube_map_negative_z = GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
+	texture_cube_map_positive_x = 0x8515,
+	texture_cube_map_positive_y = 0x8517,
+	texture_cube_map_positive_z = 0x8519,
+	texture_cube_map_negative_x = 0x8516,
+	texture_cube_map_negative_y = 0x8518,
+	texture_cube_map_negative_z = 0x851A
+};
+
+enum class TextureInternalFormat {
+	r8 = 0x8229,
+	r8_snorm = 0x8F94,
+	r16 = 0x822A,
+	r16_snorm = 0x8F98,
+	rg8 = 0x822B,
+	rg8_snorm = 0x8F95,
+	rg16 = 0x822C,
+	rg16_snorM = 0x8F99,
+	r3_g3_b2 = 0x2A10,
+	rgb4 = 0x804F,
+	rgb5 = 0x8050,
+	rgb8 = 0x8051,
+	rgb8_snorM = 0x8F96,
+	rgb10 = 0x8052,
+	rgb12 = 0x8053,
+	rgb16_snoRM = 0x8F9A,
+	rgba2 = 0x8055,
+	rgba4 = 0x8056,
+	rgb5_a1 = 0x8057,
+	rgba8 = 0x8058,
+	rgba8_snoRM = 0x8F97,
+	rgb10_a2 = 0x8059,
+	rgb10_a2uI = 0x906F,
+	rgba12 = 0x805A,
+	rgba16 = 0x805B,
+	srgb8 = 0x8C41,
+	r16f = 0x822D,
+	rg16f = 0x822F,
+	rgb16f = 0x881B,
+	rgba16f = 0x881A,
+	r32f = 0x822E,
+	rg32f = 0x8230,
+	rgb32f = 0x8815,
+	rgba32f = 0x8814,
+	rgb9_e5 = 0x8C3D,
+	r8i = 0x8231,
+	r8ui = 0x8232,
+	r16i = 0x8233,
+	r16ui = 0x8234,
+	r32i = 0x8235,
+	r32ui = 0x8236,
+	rg8i = 0x8237,
+	rg8ui = 0x8238,
+	rg16i = 0x8239,
+	rg16ui = 0x823A,
+	rg32i = 0x823B,
+	rg32ui = 0x823C,
+	rgb8i = 0x8D8F,
+	rgb8ui = 0x8D7D,
+	rgb16i = 0x8D89,
+	rgb16ui = 0x8D77,
+	rgb32i = 0x8D83,
+	rgb32ui = 0x8D71,
+	rgba8i = 0x8D8E,
+	rgba8ui = 0x8D7C,
+	rgba16i = 0x8D88,
+	rgba16ui = 0x8D76,
+	rgba32i = 0x8D82,
+	rgba32ui = 0x8D70,
+};
+
+enum class TextureFormat {
+	none = 0,
+	red = 0x1903,
+	rg = 0x8227,
+	rgb = 0x1907,
+	bgr = 0x80E0,
+	rgba = 0x1908,
+	bgra = 0x80E1,
+	depth_component = 0x1902,
+	stencil_index = 0x1901,
 };
 
 enum class TextureDepthStencilMode {
-	depth_component = GL_DEPTH_COMPONENT,
-	stencil_index = GL_STENCIL_INDEX
+	depth_component = 0x1902,
+	stencil_index = 0x1901
 };
 
 enum class TextureCompareFunc {
-	lequal = GL_LEQUAL,
-	gequal = GL_GEQUAL,
-	less = GL_LESS,
-	greater = GL_GREATER,
-	equal = GL_EQUAL,
-	notequal = GL_NOTEQUAL,
-	always = GL_ALWAYS,
-	never = GL_NEVER
+	lequal = 0x0203,
+	gequal = 0x0206,
+	less = 0x0201,
+	greater = 0x0204,
+	equal = 0x0202,
+	notequal = 0x0205,
+	always = 0x0207,
+	never = 0x0200
 };
 
 enum class TextureCompareMode {
-	compare_ref_to_texture = GL_COMPARE_REF_TO_TEXTURE,
-	none = GL_NONE
+	compare_ref_to_texture = 0x884E,
+	none = 0
 };
 
 enum class TextureMinFilter {
-	nearest = GL_NEAREST,
-	linear = GL_LINEAR,
-	nearest_mipmap_nearest = GL_NEAREST_MIPMAP_NEAREST,
-	linear_mipmap_nearest = GL_LINEAR_MIPMAP_NEAREST,
-	nearest_mipmap_linear = GL_NEAREST_MIPMAP_LINEAR,
-	linear_mipmap_linear = GL_LINEAR_MIPMAP_LINEAR
+	nearest = 0x2600,
+	linear = 0x2601,
+	nearest_mipmap_nearest = 0x2700,
+	linear_mipmap_nearest = 0x2701,
+	nearest_mipmap_linear = 0x2702,
+	linear_mipmap_linear = 0x2703
 };
 
 enum class TextureMagFilter {
-	nearest = GL_NEAREST,
-	linear = GL_LINEAR
+	nearest = 0x2600,
+	linear = 0x2601
 };
 
 enum class TextureSwizzle {
-	red = GL_RED,
-	green = GL_GREEN,
-	blue = GL_BLUE,
-	alpha = GL_ALPHA,
-	zero = GL_ZERO,
-	one = GL_ONE
+	red = 0x1903,
+	green = 0x1904,
+	blue = 0x1905,
+	alpha = 0x1906,
+	zero = 0,
+	one = 1
 };
 
 enum class TextureWrap {
-	clamp_to_edge = GL_CLAMP_TO_EDGE,
-	clamp_to_border = GL_CLAMP_TO_BORDER,
-	mirrored_repeat = GL_MIRRORED_REPEAT,
-	repeat = GL_REPEAT,
-	mirror_clamp_to_edge = GL_MIRROR_CLAMP_TO_EDGE
+	clamp_to_edge = 0x812F,
+	clamp_to_border = 0x812D,
+	mirrored_repeat = 0x8370,
+	repeat = 0x2901,
+	mirror_clamp_to_edge = 0x8743
 };
 
 class Texture {
 public:
-	Texture(TextureTarget target);
+	Texture(const Texture&) = default;
+	Texture(Texture&&) = delete;
+	auto operator=(const Texture&) -> Texture& = default;
+	auto operator=(Texture&&) -> Texture& = delete;
+	explicit Texture(TextureTarget target);
 	~Texture();
 
-public:
-	auto bind() -> Texture*;
+	void bind() const;
 	void unbind() const;
-	void activate(unsigned int slot = 0) const;
 
-	auto generate_mipmap() -> Texture*;
+	void bind_unit(unsigned int unit = 0) const;
 
-	auto set_depth_stencil_mode(TextureDepthStencilMode mode) -> Texture*;
+	void set_texture_storage_2d(
+		unsigned int levels, TextureInternalFormat internal_format, const glm::vec2& size
+	);
 
-	auto set_base_level(int level) -> Texture*;
+	void set_sub_image_2d(
+		unsigned int level, const glm::vec2& offset, const glm::vec2& size, TextureFormat format,
+		const void* pixels
+	) const;
 
-	auto set_border_color(glm::vec4 color) -> Texture*;
+	void generate_mipmap() const;
 
-	auto set_compare_func(TextureCompareFunc func) -> Texture*;
-	auto set_compare_mode(TextureCompareMode mode) -> Texture*;
+	void set_depth_stencil_mode(TextureDepthStencilMode mode) const;
 
-	auto set_lod_bias(float bias) -> Texture*;
+	void set_base_level(int level) const;
+	void set_max_level(int level) const;
 
-	auto set_min_filter(TextureMinFilter filter) -> Texture*;
-	auto set_mag_filter(TextureMagFilter filter) -> Texture*;
+	void set_border_color(const glm::vec4& color) const;
 
-	auto set_min_lod(float lod) -> Texture*;
-	auto set_max_lod(float lod) -> Texture*;
+	void set_compare_func(TextureCompareFunc func) const;
+	void set_compare_mode(TextureCompareMode mode) const;
 
-	auto set_max_level(int level) -> Texture*;
+	void set_min_filter(TextureMinFilter filter) const;
+	void set_mag_filter(TextureMagFilter filter) const;
 
-	auto set_swizzle_r(TextureSwizzle swizzle) -> Texture*;
-	auto set_swizzle_g(TextureSwizzle swizzle) -> Texture*;
-	auto set_swizzle_b(TextureSwizzle swizzle) -> Texture*;
-	auto set_swizzle_a(TextureSwizzle swizzle) -> Texture*;
+	void set_min_lod(float lod) const;
+	void set_max_lod(float lod) const;
+	void set_lod_bias(float bias) const;
 
-	auto set_wrap_s(TextureWrap wrap) -> Texture*;
-	auto set_wrap_t(TextureWrap wrap) -> Texture*;
-	auto set_wrap_r(TextureWrap wrap) -> Texture*;
+	void set_swizzle_r(TextureSwizzle swizzle) const;
+	void set_swizzle_g(TextureSwizzle swizzle) const;
+	void set_swizzle_b(TextureSwizzle swizzle) const;
+	void set_swizzle_a(TextureSwizzle swizzle) const;
 
-	void set_size(glm::vec2 size);
-	[[nodiscard]] auto get_size() const -> glm::vec2;
+	void set_wrap_s(TextureWrap wrap) const;
+	void set_wrap_t(TextureWrap wrap) const;
+	void set_wrap_r(TextureWrap wrap) const;
+
+	[[nodiscard]] auto get_size() const -> const glm::vec2&;
 
 private:
 	TextureTarget _target;
-	unsigned int _id;
-	glm::vec2 _size;
+	unsigned int _id = 0;
+	glm::vec2 _size = glm::vec2(0.0f);
 };
 
 } // namespace void_engine::resources
 
-#endif // !#ifndef VOID_ENGINE_RESOURCES_TEXTURE_HPP
+#endif // !VOID_ENGINE_RESOURCES_TEXTURE_HPP
--- src/resources/texture.cpp
+++ src/resources/texture.cpp
@@ -1,151 +1,139 @@
 #include "void_engine/resources/texture.hpp"
 
+#include <glad/glad.h>
+#include <glm/ext/vector_float2.hpp>
+#include <glm/ext/vector_float4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 namespace void_engine::resources {
 
 Texture::Texture(TextureTarget target) : _target(target) {
-	glGenTextures(1, &_id);
+	glCreateTextures(static_cast<int>(_target), 1, &_id);
 }
+
 Texture::~Texture() {
 	glDeleteTextures(1, &_id);
 }
 
-auto Texture::bind() -> Texture* {
+void Texture::bind() const {
 	glBindTexture(static_cast<int>(_target), _id);
-	return this;
 }
+
 void Texture::unbind() const {
 	glBindTexture(static_cast<int>(_target), 0);
 }
-void Texture::activate(unsigned int slot) const {
-	glActiveTexture(GL_TEXTURE0 + slot);
+
+void Texture::bind_unit(unsigned int unit) const {
+	glBindTextureUnit(unit, _id);
 }
 
-auto Texture::generate_mipmap() -> Texture* {
-	glGenerateMipmap(static_cast<int>(_target));
-	return this;
-}
-
-auto Texture::set_depth_stencil_mode(TextureDepthStencilMode mode) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_DEPTH_STENCIL_TEXTURE_MODE,
-		static_cast<int>(mode)
+void Texture::set_texture_storage_2d(
+	unsigned int levels, TextureInternalFormat internal_format, const glm::vec2& size
+) {
+	glTextureStorage2D(
+		_id,
+		static_cast<int>(levels),
+		static_cast<int>(internal_format),
+		static_cast<int>(size.x),
+		static_cast<int>(size.y)
 	);
-	return this;
-}
-
-auto Texture::set_base_level(int level) -> Texture* {
-	glTexParameteri(static_cast<int>(_target), GL_TEXTURE_BASE_LEVEL, level);
-	return this;
-}
-
-auto Texture::set_border_color(glm::vec4 color) -> Texture* {
-	glTexParameterfv(
-		static_cast<int>(_target), GL_TEXTURE_BORDER_COLOR, &color[0]
-	);
-	return this;
-}
-
-auto Texture::set_compare_func(TextureCompareFunc func) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_TEXTURE_COMPARE_FUNC,
-		static_cast<int>(func)
-	);
-	return this;
-}
-auto Texture::set_compare_mode(TextureCompareMode mode) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_TEXTURE_COMPARE_MODE,
-		static_cast<int>(mode)
-	);
-	return this;
-}
-
-auto Texture::set_lod_bias(float bias) -> Texture* {
-	glTexParameterf(static_cast<int>(_target), GL_TEXTURE_LOD_BIAS, bias);
-	return this;
-}
-
-auto Texture::set_min_filter(TextureMinFilter filter) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_TEXTURE_MIN_FILTER,
-		static_cast<int>(filter)
-	);
-	return this;
-}
-auto Texture::set_mag_filter(TextureMagFilter filter) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_TEXTURE_MAG_FILTER,
-		static_cast<int>(filter)
-	);
-	return this;
-}
-
-auto Texture::set_min_lod(float lod) -> Texture* {
-	glTexParameterf(static_cast<int>(_target), GL_TEXTURE_MIN_LOD, lod);
-	return this;
-}
-auto Texture::set_max_lod(float lod) -> Texture* {
-	glTexParameterf(static_cast<int>(_target), GL_TEXTURE_MAX_LOD, lod);
-	return this;
-}
-
-auto Texture::set_max_level(int level) -> Texture* {
-	glTexParameteri(static_cast<int>(_target), GL_TEXTURE_MAX_LEVEL, level);
-	return this;
-}
-
-auto Texture::set_swizzle_r(TextureSwizzle swizzle) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_TEXTURE_SWIZZLE_R,
-		static_cast<int>(swizzle)
-	);
-	return this;
-}
-auto Texture::set_swizzle_g(TextureSwizzle swizzle) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_TEXTURE_SWIZZLE_G,
-		static_cast<int>(swizzle)
-	);
-	return this;
-}
-auto Texture::set_swizzle_b(TextureSwizzle swizzle) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_TEXTURE_SWIZZLE_B,
-		static_cast<int>(swizzle)
-	);
-	return this;
-}
-auto Texture::set_swizzle_a(TextureSwizzle swizzle) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_TEXTURE_SWIZZLE_A,
-		static_cast<int>(swizzle)
-	);
-	return this;
-}
-
-auto Texture::set_wrap_s(TextureWrap wrap) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_TEXTURE_WRAP_S, static_cast<int>(wrap)
-	);
-	return this;
-}
-auto Texture::set_wrap_t(TextureWrap wrap) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_TEXTURE_WRAP_T, static_cast<int>(wrap)
-	);
-	return this;
-}
-auto Texture::set_wrap_r(TextureWrap wrap) -> Texture* {
-	glTexParameteri(
-		static_cast<int>(_target), GL_TEXTURE_WRAP_R, static_cast<int>(wrap)
-	);
-	return this;
-}
-
-void Texture::set_size(glm::vec2 size) {
 	_size = size;
 }
-auto Texture::get_size() const -> glm::vec2 {
+
+void Texture::set_sub_image_2d(
+	unsigned int level, const glm::vec2& offset, const glm::vec2& size, TextureFormat format,
+	const void* pixels
+) const {
+	glTextureSubImage2D(
+		_id,
+		static_cast<int>(level),
+		static_cast<int>(offset.x),
+		static_cast<int>(offset.y),
+		static_cast<int>(size.x),
+		static_cast<int>(size.y),
+		static_cast<int>(format),
+		GL_UNSIGNED_BYTE,
+		pixels
+	);
+}
+
+void Texture::generate_mipmap() const {
+	glGenerateTextureMipmap(_id);
+}
+
+void Texture::set_depth_stencil_mode(TextureDepthStencilMode mode) const {
+	glTextureParameteri(_id, GL_DEPTH_STENCIL_TEXTURE_MODE, static_cast<int>(mode));
+}
+
+void Texture::set_base_level(int level) const {
+	glTextureParameteri(_id, GL_TEXTURE_BASE_LEVEL, level);
+}
+
+void Texture::set_max_level(int level) const {
+	glTextureParameteri(_id, GL_TEXTURE_MAX_LEVEL, level);
+}
+
+void Texture::set_border_color(const glm::vec4& color) const {
+	glTextureParameterfv(_id, GL_TEXTURE_BORDER_COLOR, glm::value_ptr(color));
+}
+
+void Texture::set_compare_func(TextureCompareFunc func) const {
+	glTextureParameteri(_id, GL_TEXTURE_COMPARE_FUNC, static_cast<int>(func));
+}
+
+void Texture::set_compare_mode(TextureCompareMode mode) const {
+	glTextureParameteri(_id, GL_TEXTURE_COMPARE_MODE, static_cast<int>(mode));
+}
+
+void Texture::set_min_filter(TextureMinFilter filter) const {
+	glTextureParameteri(_id, GL_TEXTURE_MIN_FILTER, static_cast<int>(filter));
+}
+
+void Texture::set_mag_filter(TextureMagFilter filter) const {
+	glTextureParameteri(_id, GL_TEXTURE_MAG_FILTER, static_cast<int>(filter));
+}
+
+void Texture::set_min_lod(float lod) const {
+	glTextureParameterf(_id, GL_TEXTURE_MIN_LOD, lod);
+}
+
+void Texture::set_max_lod(float lod) const {
+	glTextureParameterf(_id, GL_TEXTURE_MAX_LOD, lod);
+}
+
+void Texture::set_lod_bias(float bias) const {
+	glTextureParameterf(_id, GL_TEXTURE_LOD_BIAS, bias);
+}
+
+void Texture::set_swizzle_r(TextureSwizzle swizzle) const {
+	glTextureParameteri(_id, GL_TEXTURE_SWIZZLE_R, static_cast<int>(swizzle));
+}
+
+void Texture::set_swizzle_g(TextureSwizzle swizzle) const {
+	glTextureParameteri(_id, GL_TEXTURE_SWIZZLE_G, static_cast<int>(swizzle));
+}
+
+void Texture::set_swizzle_b(TextureSwizzle swizzle) const {
+	glTextureParameteri(_id, GL_TEXTURE_SWIZZLE_B, static_cast<int>(swizzle));
+}
+
+void Texture::set_swizzle_a(TextureSwizzle swizzle) const {
+	glTextureParameteri(_id, GL_TEXTURE_SWIZZLE_A, static_cast<int>(swizzle));
+}
+
+void Texture::set_wrap_s(TextureWrap wrap) const {
+	glTextureParameteri(_id, GL_TEXTURE_WRAP_S, static_cast<int>(wrap));
+}
+
+void Texture::set_wrap_t(TextureWrap wrap) const {
+	glTextureParameteri(_id, GL_TEXTURE_WRAP_T, static_cast<int>(wrap));
+}
+
+void Texture::set_wrap_r(TextureWrap wrap) const {
+	glTextureParameteri(_id, GL_TEXTURE_WRAP_R, static_cast<int>(wrap));
+}
+
+auto Texture::get_size() const -> const glm::vec2& {
 	return _size;
 }
 
