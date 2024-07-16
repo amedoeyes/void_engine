26955fc9a2448ea43c33a9822460e73abaf3bef4
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-07-16 13:19:55 +0300

    feat: add the ability to set root path in the shader manager

--- include/void_engine/resources/shader_manager.hpp
+++ include/void_engine/resources/shader_manager.hpp
@@ -3,6 +3,7 @@
 
 #include "void_engine/resources/shader.hpp"
 
+#include <filesystem>
 #include <string>
 #include <unordered_map>
 
@@ -15,14 +16,18 @@ public:
 	auto operator=(const ShaderManager&) -> ShaderManager& = default;
 	auto operator=(ShaderManager&&) -> ShaderManager& = delete;
 	ShaderManager() = default;
+	explicit ShaderManager(std::filesystem::path root_path);
 	~ShaderManager();
 
 	auto create(std::string_view name) -> Shader&;
 	void destroy(std::string_view name);
 	auto get(std::string_view name) -> Shader&;
 
+	void set_root_path(const std::filesystem::path& root_path);
+
 private:
 	std::unordered_map<std::string, Shader*> _shaders;
+	std::filesystem::path _root_path;
 };
 
 } // namespace void_engine::resources
--- src/resources/shader_manager.cpp
+++ src/resources/shader_manager.cpp
@@ -8,6 +8,9 @@
 
 namespace void_engine::resources {
 
+ShaderManager::ShaderManager(std::filesystem::path root_path) : _root_path(std::move(root_path)) {
+}
+
 ShaderManager::~ShaderManager() {
 	for (auto& [_, shader] : _shaders) {
 		delete shader;
@@ -16,7 +19,7 @@ ShaderManager::~ShaderManager() {
 
 auto ShaderManager::create(std::string_view name) -> Shader& {
 	assert(_shaders.find(name.data()) == _shaders.end() && "Shader already exists");
-	auto [it, _] = _shaders.emplace(name.data(), new Shader);
+	auto [it, _] = _shaders.emplace(name.data(), new Shader(_root_path));
 	return *(it->second);
 }
 
@@ -31,6 +34,13 @@ auto ShaderManager::get(std::string_view name) -> Shader& {
 	const auto it = _shaders.find(name.data());
 	assert(it != _shaders.end() && "Texture does not exist");
 	return *(it->second);
+}
+
+void ShaderManager::set_root_path(const std::filesystem::path& root_path) {
+	_root_path = root_path;
+	for (auto& [_, shader] : _shaders) {
+		shader->set_root_path(_root_path);
+	}
 }
 
 } // namespace void_engine::resources
