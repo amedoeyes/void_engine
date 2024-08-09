a58ad228459a0b71efe5616aa7824b9f7e15e0a4
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-08-09 22:36:05 +0300

    feat: add mesh class

---
+++ include/void_engine/renderer/mesh.hpp
@@ -0,0 +1,32 @@
+#ifndef VOID_ENGINE_RENDERER_MESH_HPP
+#define VOID_ENGINE_RENDERER_MESH_HPP
+
+#include "void_engine/renderer/buffer/element_buffer.hpp"
+#include "void_engine/renderer/buffer/vertex_buffer.hpp"
+#include "void_engine/renderer/renderer.hpp"
+#include "void_engine/renderer/vertex_array.hpp"
+
+namespace void_engine::renderer {
+
+class Mesh {
+public:
+	Mesh(const Mesh&) = default;
+	Mesh(Mesh&&) = default;
+	auto operator=(const Mesh&) -> Mesh& = default;
+	auto operator=(Mesh&&) -> Mesh& = default;
+	Mesh() = delete;
+	Mesh(VertexArray vao, buffer::VertexBuffer vbo, buffer::ElementBuffer ebo, PrimitiveType type);
+	~Mesh() = default;
+
+	void draw();
+
+private:
+	VertexArray _vao;
+	buffer::VertexBuffer _vbo;
+	buffer::ElementBuffer _ebo;
+	PrimitiveType _type;
+};
+
+} // namespace void_engine::renderer
+
+#endif // !VOID_ENGINE_RENDERER_MESH_HPP
